473a48f6188118c65dff01bb13e4f1bf15fe0946
DeemahYousef <dalkhaledi12345@hotmail.com>
2022-03-05 13:52:56 +0300

    done

---
+++ ios-cw-4/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+let name = "Deemah"
+
+var hobby = "baking"
+
+var favouriteNumber = 7
+
+print("السلام عليكم، معاكم \(name)ورقمي المفضل اهو \(favouriteNumber)واحب ال\(hobby)")
+
+var hight = 1.64
